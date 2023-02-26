09d211ce16fd40f4acb40b4aaf29b19a3f6a475c
Mati <maticristovao@hotmail.com>
2023-02-26 10:39:59 -0300

    footer draft

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
+import { faEnvelope, faEnvelopeOpenText } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class FooterComponent {
-
+  faEnvelope = faEnvelope;
+  faOpen = faEnvelopeOpenText;
 }
