616c81faea99f55f04c80913f98b790335224e6f
Mati <maticristovao@hotmail.com>
2023-03-01 11:59:43 -0300

    toast login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { faArrowLeft, faEye, faEyeSlash, faKey, faUserShield } from '@fortawesome/free-solid-svg-icons';
+import { IndividualConfig, ToastrService } from 'ngx-toastr';
 import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
@@ -11,13 +12,14 @@ import { AuthService } from 'src/app/services/auth.service';
 export class LoginComponent {
   form: FormGroup;
   show: boolean = false;
+  logToastConfig: Partial<IndividualConfig> = { positionClass: 'log-toast-container', progressBar: false };
   faBack = faArrowLeft;
   faUsername = faUserShield;
   faKey = faKey;
   faEye = faEye;
   faEyeSlash = faEyeSlash;
 
-  constructor(private authService: AuthService, private formBuilder: FormBuilder){
+  constructor(private authService: AuthService, private formBuilder: FormBuilder, private toastService: ToastrService) {
     this.form = this.formBuilder.group({
       username: ['', [Validators.required]],
       password: ['', [Validators.required]],
@@ -34,13 +36,19 @@ export class LoginComponent {
   get Remember() {
     return this.form.get('remember');
   }
-  
+
   toggleVisibility() {
     this.show = !this.show;
   }
 
-  logIn(logData:LoginData){
+  logIn(logData: LoginData) {
     this.authService.login(logData.username, logData.password, logData.remember);
+
+    if (this.authService.isLogged) {
+      this.toastService.success('Inicio de sesión exitoso', 'Log In', this.logToastConfig);
+    } else {
+      this.toastService.error('Credenciales inválidas', 'Log In', this.logToastConfig);
+    }
   }
 
   onSubmit() {
@@ -53,7 +61,7 @@ export class LoginComponent {
     }
   }
 }
-interface LoginData{
+interface LoginData {
   username: string,
   password: string,
   remember: boolean
