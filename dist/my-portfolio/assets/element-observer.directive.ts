00590a20c3675dee4243d3100d04d7d499dd89a8
Mati <maticristovao@hotmail.com>
2023-06-29 16:07:29 -0300

    dist out from gitignore

---
+++ dist/my-portfolio/assets/element-observer.directive.ts
@@ -0,0 +1,65 @@
+import { Directive, Input, OnInit, ElementRef, EventEmitter, Output } from '@angular/core';
+import { Subject } from 'rxjs';
+
+@Directive({
+  selector: '[elementObserver]',
+  exportAs: 'intersection'
+})
+export class ElementObserverDirective implements OnInit {
+  @Input() threshold: number = 0;
+  @Input() margin: string = '0px';
+  @Input() wait: number = 0;
+  @Output() visible = new EventEmitter<HTMLElement>();
+
+  timer = (ms: number | undefined) => new Promise(res => setTimeout(res, ms));
+
+  private observer?: IntersectionObserver;
+
+  private subject$ = new Subject<{
+    entry: IntersectionObserverEntry;
+    observer: IntersectionObserver;
+  }>();
+
+  constructor(private element: ElementRef) { }
+
+  ngOnInit() {
+    this.createObserver();
+  }
+
+  ngAfterViewInit() {
+    this.startObservingElements();
+  }
+
+  private createObserver() {
+    const options = {
+      rootMargin: this.margin,
+      threshold: this.threshold,
+    };
+
+    const isIntersecting = (entry: IntersectionObserverEntry) =>
+      entry.isIntersecting;
+
+    this.observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach(async entry => {
+        if (isIntersecting(entry)) {
+          await this.timer(this.wait);
+          this.subject$.next({ entry, observer });
+        }
+      });
+    }, options);
+  }
+
+  private startObservingElements() {
+    if (!this.observer) {
+      return;
+    }
+
+    this.observer.observe(this.element.nativeElement);
+
+    this.subject$.subscribe(({ entry, observer }) => {
+      const target = entry.target as HTMLElement;
+      this.visible.emit(target);
+      observer.unobserve(target);
+    });
+  }
+}
---
+++ dist/my-portfolio/assets/section.ts
@@ -0,0 +1,58 @@
+import { Directive, HostListener, OnInit } from "@angular/core";
+import { faTrashCanArrowUp } from "@fortawesome/free-solid-svg-icons";
+import { ToastrService } from "ngx-toastr";
+import { AddItemComponent } from "src/app/components/add-item/add-item.component";
+import { AuthService } from "src/app/services/auth.service";
+import { PersonalInfoService } from "src/app/services/personal-info.service";
+
+@Directive()
+export abstract class Section implements OnInit {
+    // Esto cambia cuando pase la app a inglés, ahí uso lo mismo que para hacer los http request
+    campo: string = '';
+    windowWidth: number = window.innerWidth;
+    abstract editModal: AddItemComponent;
+    field: string = '';
+    editMode: boolean = false;
+    visible: boolean = false;
+    itemDelIcon = faTrashCanArrowUp;
+
+    @HostListener('window:resize')
+    onResize(): void {
+        this.windowWidth = window.innerWidth;
+    }
+
+    constructor(protected personalData: PersonalInfoService, protected toastService: ToastrService, protected authService: AuthService) { }
+
+    abstract getData(): void;
+
+    toggleModal() {
+        this.editModal.reset();
+        this.editModal.open(this.editModal.myModal);
+    }
+    passData(item: any) {
+        this.toggleModal();
+        this.editModal.loadEditData!(item);
+    }
+
+    showSuccess(type: 'add' | 'edit', title?: string) {
+        if (title) {
+            this.campo = title;
+        }
+        if (type === 'add') {
+            this.toastService.success('Ítem añadido correctamente', this.campo);
+        } else {
+            this.toastService.success('Cambios guardados', this.campo);
+        }
+    }
+
+    showDelete(title?: string) {
+        if (title) {
+            this.campo = title;
+        }
+        this.toastService.error('Ítem eliminado', this.campo, { toastClass: 'toast-delete ngx-toastr' });
+    }
+
+    ngOnInit(): void {
+        this.getData();
+    }
+}
